08520c570bba7ba34cbfc239c2a0f111893ca98b
Tobias Widlund <widlundtobias@gmail.com>
2014-01-16 00:50:56 +0800

    rain can now destroy ground

--- rain/src/main.cpp
+++ rain/src/main.cpp
@@ -14,10 +14,14 @@ int main()
 
     fea::Texture groundTexture;
     fea::Texture skyTexture;
+    fea::Texture rainTexture;
+    fea::Texture rainSplashTexture;
 
     ImageLoader loader;
     groundTexture.create(800, 600, loader.loadImage("raindata/textures/ground.png", 800, 600).data(), false, true);
     skyTexture.create(800, 600, loader.loadImage("raindata/textures/sky.png", 800, 600).data());
+    rainTexture.create(20, 20, loader.loadImage("raindata/textures/raindrop.png", 20, 20).data());
+    rainSplashTexture.create(13, 13, loader.loadImage("raindata/textures/rainsplash.png", 13, 13).data());
 
     fea::Quad ground(800, 600);
     ground.setTexture(groundTexture);
@@ -26,6 +30,12 @@ int main()
 
     fea::Renderer2D renderer(fea::Viewport(800, 600, 0, 0, fea::Camera(800.0f/2.0f, 600.0f/2.0f)));
     renderer.setup();
+
+    RainHandler rainHandler;
+    fea::Quad rainDrop(20.0f, 20.0f);
+    rainDrop.setTexture(rainTexture);
+    fea::Quad rainSplash(13.0f, 13.0f);
+    rainSplash.setTexture(rainSplashTexture);
 
     bool quit = false;
     while(!quit)
@@ -42,9 +52,49 @@ int main()
             }
         }
 
+        rainHandler.update(groundTexture);
+
+        auto collisions = rainHandler.getCollisions();
+
+        for(const auto& collision : collisions)
+            groundTexture.setPixels([&] (uint32_t w, uint32_t h, uint8_t* data) 
+            {
+                (void)h;
+                uint32_t startX = collision.x - 6;
+                uint32_t startY = collision.y - 6;
+
+                for(uint32_t x = 0; x < 12; x++)
+                {
+                    for(uint32_t y = 0; y < 12; y++)
+                    {
+                        if(glm::distance(glm::vec2(startX + x, startY + y), collision) < 6.0f)
+                        data[((startX + x) + (startY + y) * w) * 4 + 3] = 0;
+                    }
+                }
+            });
+
+        if(collisions.size() > 0)
+            groundTexture.update();
+
         renderer.clear();
         renderer.queue(sky);
         renderer.queue(ground);
+        for(auto position : rainHandler.getRaindropPositions())
+        {
+            rainDrop.setPosition(position);
+            renderer.queue(rainDrop);
+        }
+
+        auto splashPositions = rainHandler.getRainsplashPositions();
+        auto splashSizes = rainHandler.getRainsplashSizes();
+
+        for(uint32_t i = 0; i < splashPositions.size(); i++)
+        {
+            rainSplash.setPosition(splashPositions[i]);
+            rainSplash.setScale({splashSizes[i], splashSizes[i]});
+            renderer.queue(rainSplash);
+        }
+
         renderer.render();
         window.swapBuffers();
     }
--- rain/src/rainhandler.cpp
+++ rain/src/rainhandler.cpp
@@ -1,5 +1,97 @@
 #include "rainhandler.h"
+#include <cstdlib>
+#include <iostream>
 
-class RainHandler
+void RainHandler::update(const fea::Texture& solidMask)
 {
-};
+    mCollisions.clear();
+
+    //move current drops
+    for(auto& drop : mRaindrops)
+    {
+        drop.x -= 6.0f;
+        drop.y += 6.0f;
+    }
+
+    //create new drops
+    if(!(rand() % 3))
+        mRaindrops.push_back({rand() % 1500, -20});
+
+    //check for border removal and collisions
+    for(auto iterator = mRaindrops.begin(); iterator != mRaindrops.end();)
+    {
+        if(iterator->x < -20)
+        {
+            iterator = mRaindrops.erase(iterator);
+            continue;
+        }
+        
+        uint32_t pixelX = std::max(0, std::min((int32_t)(iterator->x + 10.0f), 800));
+        uint32_t pixelY = std::max(0, std::min((int32_t)(iterator->y + 18.0f), 600));
+        
+        if(solidMask.getPixel(pixelX, pixelY).a != 0.0f)
+        {
+            iterator = mRaindrops.erase(iterator);
+
+            if(pixelX > 0 && pixelX < 800 && pixelY > 0 && pixelY < 600)
+            {
+                mCollisions.push_back({pixelX, pixelY});
+
+                for(uint32_t i = 0; i < 5; i++)
+                {
+                    Rainsplash newSplash({{pixelX - 6, pixelY - 10}, {((float)(rand() % 21) / 4.0f) - 2.5f, ((rand() % 21) / 4.0f) - 2.5f}, static_cast<uint32_t>((rand() % 60) + 40)});
+                    mRainsplashes.push_back(newSplash);
+                }
+            }
+            continue;
+        }
+
+        iterator++;
+    }
+
+    //animate splashes
+    for(auto iterator = mRainsplashes.begin(); iterator != mRainsplashes.end();)
+    {
+        iterator->ttl -= 5;
+
+        if(iterator->ttl > 100)
+        {
+            iterator = mRainsplashes.erase(iterator);
+            continue;
+        }
+
+        iterator->mPos += iterator->mVel;
+
+        iterator++;
+    }
+}
+
+std::vector<glm::vec2> RainHandler::getRaindropPositions() const
+{
+    return mRaindrops;
+}
+
+std::vector<glm::vec2> RainHandler::getRainsplashPositions() const
+{
+    std::vector<glm::vec2> result;
+
+    for(auto& splash : mRainsplashes)
+        result.push_back(splash.mPos);
+
+    return result;
+}
+
+std::vector<float> RainHandler::getRainsplashSizes() const
+{
+    std::vector<float> result;
+
+    for(auto& splash : mRainsplashes)
+        result.push_back((float)splash.ttl / 100.0f);
+
+    return result;
+}
+
+std::vector<glm::vec2> RainHandler::getCollisions() const
+{
+    return mCollisions;
+}
