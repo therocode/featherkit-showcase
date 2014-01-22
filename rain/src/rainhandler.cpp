08d39b36db40277d1d32bd58fea7db415c083653
Tobias Widlund <widlundtobias@gmail.com>
2014-01-22 14:20:45 +0800

    fixed

--- rain/src/rainhandler.cpp
+++ rain/src/rainhandler.cpp
@@ -2,34 +2,34 @@
 #include <cstdlib>
 #include <iostream>
 
-void RainHandler::update(const fea::Texture& solidMask)
-{
-    mCollisions.clear();
-
-    //move current drops
-    for(auto& drop : mRaindrops)
+    void RainHandler::update(const fea::Texture& solidMask)
     {
-        drop.x -= 6.0f;
-        drop.y += 6.0f;
-    }
+        mCollisions.clear();
 
-    //create new drops
-    if(!(rand() % 3))
-        mRaindrops.push_back(glm::vec2(rand() % 3000, -20));
-
-    //check for border removal and collisions
-    for(auto iterator = mRaindrops.begin(); iterator != mRaindrops.end();)
-    {
-        if(iterator->x < -20)
+        //move current drops
+        for(auto& drop : mRaindrops)
         {
-            iterator = mRaindrops.erase(iterator);
-            continue;
+            drop.x -= 6.0f;
+            drop.y += 6.0f;
         }
-        
-        uint32_t pixelX = std::max(0, std::min((int32_t)(iterator->x + 10.0f), 1600)) / 2;
-        uint32_t pixelY = std::max(0, std::min((int32_t)(iterator->y + 18.0f), 1200)) / 2;
-        
-        if(solidMask.getPixel(pixelX, pixelY).a != 0.0f)
+
+        //create new drops
+        if(!(rand() % 3))
+            mRaindrops.push_back(glm::vec2(rand() % 3000, -20));
+
+        //check for border removal and collisions
+        for(auto iterator = mRaindrops.begin(); iterator != mRaindrops.end();)
+        {
+            if(iterator->x < -20)
+            {
+                iterator = mRaindrops.erase(iterator);
+                continue;
+            }
+            
+            uint32_t pixelX = std::max(0, std::min((int32_t)(iterator->x + 10.0f), 1600)) / 2;
+            uint32_t pixelY = std::max(0, std::min((int32_t)(iterator->y + 18.0f), 1200)) / 2;
+            
+            if(solidMask.getPixel(pixelX, pixelY).a() != 0.0f)
         {
             iterator = mRaindrops.erase(iterator);
 
