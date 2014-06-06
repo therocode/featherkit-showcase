1076b279fdb81e2fd2798fd8dbf822ae1da3844f
kim <kimspindel@gmail.com>
2014-06-06 17:48:06 +0800

    removed the old "rain" showcase

--- rain/src/imageloader.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <vector>
-#include <string>
-
-class ImageLoader
-{
-    public:
-        std::vector<unsigned char> loadImage(std::string path, uint32_t width, uint32_t height);
-};
